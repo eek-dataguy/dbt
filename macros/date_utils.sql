f13de946a045622824dd82c36f631737a714bde1
E Ek <eek.datasc@gmail.com>
2026-05-29 04:01:01 +0000

    get season and day type macro

---
+++ macros/date_utils.sql
@@ -0,0 +1,20 @@
+{% macro get_season(date) %}
+    CASE 
+            WHEN MONTH(TO_TIMESTAMP({{date}})) IN (12,1,2) 
+                THEN 'WINTER'
+            WHEN MONTH(TO_TIMESTAMP({{date}})) IN (3,4,5) 
+                THEN 'SPRING'
+            WHEN MONTH(TO_TIMESTAMP({{date}})) IN (6,7,8) 
+                THEN 'SUMMER'
+            ELSE 'AUTUMN'
+    END
+{% endmacro %}
+
+
+{% macro day_type(date) %}
+    CASE
+        WHEN DAYNAME(TO_TIMESTAMP({{date}})) IN ('Sat', 'Sun') 
+            THEN 'WEEKEND'
+        ELSE 'BUSINESSDAY'
+        END
+{% endmacro %}
